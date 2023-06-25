a21cca59983cf88cfdaacb413638e6f260224927
Steven Kalt <kalt.steven@gmail.com>
2023-06-25 17:25:44 -0400

    fix: ignore extraneous whitespace in title

--- assets/bookmarklets/retitle_tab.ts
+++ assets/bookmarklets/retitle_tab.ts
@@ -1,5 +1,7 @@
 document.title =
   prompt(
     "retitle tab to:",
-    document.querySelector("h1")?.textContent || document.title
+    (document.querySelector("h1")?.textContent || document.title)
+      .trim()
+      .replace(/\s+/g, " ")
   ) ?? document.title;
